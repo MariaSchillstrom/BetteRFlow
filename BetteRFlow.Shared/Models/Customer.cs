01d56d26ba7604a123afdd1a6ba8b7df1a31c9d2
maria schillstrom <mariacschillstrom@gmail.com>
2025-11-30 20:02:37 +0100

    CustomerController samt Customer.cs klara

--- BetteRFlow.Shared/Models/Customer.cs
+++ BetteRFlow.Shared/Models/Customer.cs
@@ -3,5 +3,9 @@
     public class Customer
     {
         public int ID { get; set; }
+        public string BusinessName { get; set; }
+        public string CustomerNumber { get; set; }  
+        public DateTime CreatedAt { get; set; }
+
     }
 }
