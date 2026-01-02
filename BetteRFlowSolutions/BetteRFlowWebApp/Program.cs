49e9e3807ff55220fe227dd58ce4383a1493482f
maria schillstrom <mariacschillstrom@gmail.com>
2026-01-02 14:25:57 +0100

    "Disable HTTPS redirection in production for WebApp"

--- BetteRFlowSolutions/BetteRFlowWebApp/Program.cs
+++ BetteRFlowSolutions/BetteRFlowWebApp/Program.cs
@@ -25,7 +25,10 @@ if (!app.Environment.IsDevelopment())
     app.UseHsts();
 }
 
-app.UseHttpsRedirection();
+if (!app.Environment.IsProduction())
+{
+    app.UseHttpsRedirection();
+}
 app.UseStaticFiles();
 app.UseAntiforgery();
 
